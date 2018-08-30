e91374d4d7d07014f432b40c7557fd33709858ba
rvnlvrz <rvnlvrz@gmail.com>
2018-08-30 15:55:56 +0800

    Add entity framework model

---
+++ Archive-System/ArchiveModel.edmx.sql
@@ -0,0 +1,344 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 08/30/2018 15:32:51
+-- Generated from EDMX file: C:\Users\Arvin\source\repos\Archive-System\Archive-System\ArchiveModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [Archive-SystemSecurity];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_MembershipEntity_Application]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Memberships] DROP CONSTRAINT [FK_MembershipEntity_Application];
+GO
+IF OBJECT_ID(N'[dbo].[FK_RoleEntity_Application]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Roles] DROP CONSTRAINT [FK_RoleEntity_Application];
+GO
+IF OBJECT_ID(N'[dbo].[FK_User_Application]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Users] DROP CONSTRAINT [FK_User_Application];
+GO
+IF OBJECT_ID(N'[dbo].[FK_MembershipEntity_User]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Memberships] DROP CONSTRAINT [FK_MembershipEntity_User];
+GO
+IF OBJECT_ID(N'[dbo].[FK_ProfileEntity_User]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Profiles] DROP CONSTRAINT [FK_ProfileEntity_User];
+GO
+IF OBJECT_ID(N'[dbo].[FK_UsersInRoles_Roles]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[UsersInRoles] DROP CONSTRAINT [FK_UsersInRoles_Roles];
+GO
+IF OBJECT_ID(N'[dbo].[FK_UsersInRoles_Users]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[UsersInRoles] DROP CONSTRAINT [FK_UsersInRoles_Users];
+GO
+IF OBJECT_ID(N'[dbo].[FK_AuthorUser]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Authors] DROP CONSTRAINT [FK_AuthorUser];
+GO
+IF OBJECT_ID(N'[dbo].[FK_AuthorDocument]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Authors] DROP CONSTRAINT [FK_AuthorDocument];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Applications]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Applications];
+GO
+IF OBJECT_ID(N'[dbo].[Memberships]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Memberships];
+GO
+IF OBJECT_ID(N'[dbo].[Profiles]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Profiles];
+GO
+IF OBJECT_ID(N'[dbo].[Roles]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Roles];
+GO
+IF OBJECT_ID(N'[dbo].[Users]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Users];
+GO
+IF OBJECT_ID(N'[dbo].[Documents]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Documents];
+GO
+IF OBJECT_ID(N'[dbo].[Authors]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Authors];
+GO
+IF OBJECT_ID(N'[dbo].[UsersInRoles]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[UsersInRoles];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Applications'
+CREATE TABLE [dbo].[Applications] (
+    [ApplicationId] uniqueidentifier  NOT NULL,
+    [ApplicationName] nvarchar(235)  NOT NULL,
+    [Description] nvarchar(256)  NULL
+);
+GO
+
+-- Creating table 'Memberships'
+CREATE TABLE [dbo].[Memberships] (
+    [UserId] uniqueidentifier  NOT NULL,
+    [ApplicationId] uniqueidentifier  NOT NULL,
+    [Password] nvarchar(128)  NOT NULL,
+    [PasswordFormat] int  NOT NULL,
+    [PasswordSalt] nvarchar(128)  NOT NULL,
+    [Email] nvarchar(256)  NULL,
+    [PasswordQuestion] nvarchar(256)  NULL,
+    [PasswordAnswer] nvarchar(128)  NULL,
+    [IsApproved] bit  NOT NULL,
+    [IsLockedOut] bit  NOT NULL,
+    [CreateDate] datetime  NOT NULL,
+    [LastLoginDate] datetime  NOT NULL,
+    [LastPasswordChangedDate] datetime  NOT NULL,
+    [LastLockoutDate] datetime  NOT NULL,
+    [FailedPasswordAttemptCount] int  NOT NULL,
+    [FailedPasswordAttemptWindowStart] datetime  NOT NULL,
+    [FailedPasswordAnswerAttemptCount] int  NOT NULL,
+    [FailedPasswordAnswerAttemptWindowsStart] datetime  NOT NULL,
+    [Comment] nvarchar(256)  NULL
+);
+GO
+
+-- Creating table 'Profiles'
+CREATE TABLE [dbo].[Profiles] (
+    [UserId] uniqueidentifier  NOT NULL,
+    [PropertyNames] nvarchar(max)  NOT NULL,
+    [PropertyValueStrings] nvarchar(max)  NOT NULL,
+    [PropertyValueBinary] varbinary(max)  NOT NULL,
+    [LastUpdatedDate] datetime  NOT NULL
+);
+GO
+
+-- Creating table 'Roles'
+CREATE TABLE [dbo].[Roles] (
+    [RoleId] uniqueidentifier  NOT NULL,
+    [ApplicationId] uniqueidentifier  NOT NULL,
+    [RoleName] nvarchar(256)  NOT NULL,
+    [Description] nvarchar(256)  NULL
+);
+GO
+
+-- Creating table 'Users'
+CREATE TABLE [dbo].[Users] (
+    [UserId] uniqueidentifier  NOT NULL,
+    [ApplicationId] uniqueidentifier  NOT NULL,
+    [UserName] nvarchar(50)  NOT NULL,
+    [IsAnonymous] bit  NOT NULL,
+    [LastActivityDate] datetime  NOT NULL,
+    [FirstName] nvarchar(50)  NULL,
+    [MiddleName] nvarchar(50)  NULL,
+    [LastName] nvarchar(50)  NULL
+);
+GO
+
+-- Creating table 'Documents'
+CREATE TABLE [dbo].[Documents] (
+    [ID] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(50)  NOT NULL,
+    [File] varbinary(max)  NOT NULL,
+    [Extension] nvarchar(20)  NOT NULL
+);
+GO
+
+-- Creating table 'Authors'
+CREATE TABLE [dbo].[Authors] (
+    [ID] int IDENTITY(1,1) NOT NULL,
+    [UserID] uniqueidentifier  NOT NULL,
+    [DocumentID] int  NOT NULL
+);
+GO
+
+-- Creating table 'UsersInRoles'
+CREATE TABLE [dbo].[UsersInRoles] (
+    [Roles_RoleId] uniqueidentifier  NOT NULL,
+    [Users_UserId] uniqueidentifier  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [ApplicationId] in table 'Applications'
+ALTER TABLE [dbo].[Applications]
+ADD CONSTRAINT [PK_Applications]
+    PRIMARY KEY CLUSTERED ([ApplicationId] ASC);
+GO
+
+-- Creating primary key on [UserId] in table 'Memberships'
+ALTER TABLE [dbo].[Memberships]
+ADD CONSTRAINT [PK_Memberships]
+    PRIMARY KEY CLUSTERED ([UserId] ASC);
+GO
+
+-- Creating primary key on [UserId] in table 'Profiles'
+ALTER TABLE [dbo].[Profiles]
+ADD CONSTRAINT [PK_Profiles]
+    PRIMARY KEY CLUSTERED ([UserId] ASC);
+GO
+
+-- Creating primary key on [RoleId] in table 'Roles'
+ALTER TABLE [dbo].[Roles]
+ADD CONSTRAINT [PK_Roles]
+    PRIMARY KEY CLUSTERED ([RoleId] ASC);
+GO
+
+-- Creating primary key on [UserId] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [PK_Users]
+    PRIMARY KEY CLUSTERED ([UserId] ASC);
+GO
+
+-- Creating primary key on [ID] in table 'Documents'
+ALTER TABLE [dbo].[Documents]
+ADD CONSTRAINT [PK_Documents]
+    PRIMARY KEY CLUSTERED ([ID] ASC);
+GO
+
+-- Creating primary key on [ID] in table 'Authors'
+ALTER TABLE [dbo].[Authors]
+ADD CONSTRAINT [PK_Authors]
+    PRIMARY KEY CLUSTERED ([ID] ASC);
+GO
+
+-- Creating primary key on [Roles_RoleId], [Users_UserId] in table 'UsersInRoles'
+ALTER TABLE [dbo].[UsersInRoles]
+ADD CONSTRAINT [PK_UsersInRoles]
+    PRIMARY KEY CLUSTERED ([Roles_RoleId], [Users_UserId] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [ApplicationId] in table 'Memberships'
+ALTER TABLE [dbo].[Memberships]
+ADD CONSTRAINT [FK_MembershipEntity_Application]
+    FOREIGN KEY ([ApplicationId])
+    REFERENCES [dbo].[Applications]
+        ([ApplicationId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_MembershipEntity_Application'
+CREATE INDEX [IX_FK_MembershipEntity_Application]
+ON [dbo].[Memberships]
+    ([ApplicationId]);
+GO
+
+-- Creating foreign key on [ApplicationId] in table 'Roles'
+ALTER TABLE [dbo].[Roles]
+ADD CONSTRAINT [FK_RoleEntity_Application]
+    FOREIGN KEY ([ApplicationId])
+    REFERENCES [dbo].[Applications]
+        ([ApplicationId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_RoleEntity_Application'
+CREATE INDEX [IX_FK_RoleEntity_Application]
+ON [dbo].[Roles]
+    ([ApplicationId]);
+GO
+
+-- Creating foreign key on [ApplicationId] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [FK_User_Application]
+    FOREIGN KEY ([ApplicationId])
+    REFERENCES [dbo].[Applications]
+        ([ApplicationId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_User_Application'
+CREATE INDEX [IX_FK_User_Application]
+ON [dbo].[Users]
+    ([ApplicationId]);
+GO
+
+-- Creating foreign key on [UserId] in table 'Memberships'
+ALTER TABLE [dbo].[Memberships]
+ADD CONSTRAINT [FK_MembershipEntity_User]
+    FOREIGN KEY ([UserId])
+    REFERENCES [dbo].[Users]
+        ([UserId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [UserId] in table 'Profiles'
+ALTER TABLE [dbo].[Profiles]
+ADD CONSTRAINT [FK_ProfileEntity_User]
+    FOREIGN KEY ([UserId])
+    REFERENCES [dbo].[Users]
+        ([UserId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Roles_RoleId] in table 'UsersInRoles'
+ALTER TABLE [dbo].[UsersInRoles]
+ADD CONSTRAINT [FK_UsersInRoles_Roles]
+    FOREIGN KEY ([Roles_RoleId])
+    REFERENCES [dbo].[Roles]
+        ([RoleId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating foreign key on [Users_UserId] in table 'UsersInRoles'
+ALTER TABLE [dbo].[UsersInRoles]
+ADD CONSTRAINT [FK_UsersInRoles_Users]
+    FOREIGN KEY ([Users_UserId])
+    REFERENCES [dbo].[Users]
+        ([UserId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UsersInRoles_Users'
+CREATE INDEX [IX_FK_UsersInRoles_Users]
+ON [dbo].[UsersInRoles]
+    ([Users_UserId]);
+GO
+
+-- Creating foreign key on [UserID] in table 'Authors'
+ALTER TABLE [dbo].[Authors]
+ADD CONSTRAINT [FK_AuthorUser]
+    FOREIGN KEY ([UserID])
+    REFERENCES [dbo].[Users]
+        ([UserId])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_AuthorUser'
+CREATE INDEX [IX_FK_AuthorUser]
+ON [dbo].[Authors]
+    ([UserID]);
+GO
+
+-- Creating foreign key on [DocumentID] in table 'Authors'
+ALTER TABLE [dbo].[Authors]
+ADD CONSTRAINT [FK_AuthorDocument]
+    FOREIGN KEY ([DocumentID])
+    REFERENCES [dbo].[Documents]
+        ([ID])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_AuthorDocument'
+CREATE INDEX [IX_FK_AuthorDocument]
+ON [dbo].[Authors]
+    ([DocumentID]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
