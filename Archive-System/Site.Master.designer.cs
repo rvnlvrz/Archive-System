9cea4484d8265c54719aa46f9c317d843aa8e446
rvnlvrz <rvnlvrz@gmail.com>
2018-09-20 13:44:52 +0800

    Hide upload navlink for anonymous users

--- Archive-System/Site.Master.designer.cs
+++ Archive-System/Site.Master.designer.cs
@@ -58,6 +58,15 @@ namespace Archive_System {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl navbar;
         
         /// <summary>
+        /// LoginView2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LoginView LoginView2;
+        
+        /// <summary>
         /// LoginView1 control.
         /// </summary>
         /// <remarks>
